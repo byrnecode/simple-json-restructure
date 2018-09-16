7d24bb04f82719e3886adf1b4d250b489a2d5ec6
Byrne Gabrielle Libanan <byrne.code@gmail.com>
2018-09-17 04:25:27 +0800

    Added feature to combine multiple files from a directory

--- group-by-stock.js
+++ group-by-stock.js
@@ -1,4 +1,5 @@
-const fs = require('fs')
+const fs = require('fs');
+const dir = require('node-dir');
 
 const groupBy = (items, key) => items.reduce(
     (result, item) => ({
@@ -11,31 +12,45 @@ const groupBy = (items, key) => items.reduce(
     {},
 );
 
-const inFileName = process.argv[2]
-const outFileName = process.argv[3]
+const dirRawFiles = 'raw';
+const combinedRaw = '_combined-raw.json';
+let tempArr = [];
 
-const json = JSON.parse(fs.readFileSync(inFileName, 'utf8'));
-const groupedStock = groupBy(json,'stock')
-const groupedDate = {}
-for(s in groupedStock)
-    groupedDate[s] = groupBy(groupedStock[s],'date')
+dir.readFiles(dirRawFiles,{
+    match: /.json$/, // match only filenames with a .json extension
+    exclude: /^\./ // exclude filenames that starts with a '.' (dot)
+    },
+    function(err, content, next) {
+        if (err) throw err;
+        tempArr = tempArr.concat(JSON.parse(content));
+        next();
+    },
+    function(err, files){
+        if (err) throw err;
+        fs.writeFileSync(combinedRaw, JSON.stringify(tempArr,null,2));
 
-const filtered = {}
-for(s in groupedDate){
-    filtered[s] = {}
-    for (d in groupedDate[s]) {
-        filtered[s][d] = {
-            price: groupedDate[s][d][0].price,
-            volume: groupedDate[s][d][0].volume
+        const groupedStock = groupBy(tempArr,'stock')
+        const groupedDate = {}
+        for (s in groupedStock)
+            groupedDate[s] = groupBy(groupedStock[s],'date')
+
+        const filtered = {}
+        for (s in groupedDate){
+            filtered[s] = {}
+            for (d in groupedDate[s]) {
+                filtered[s][d] = {
+                    price: groupedDate[s][d][0].price,
+                    volume: groupedDate[s][d][0].volume
+                }
+            }
         }
-    }
-}
+        // const res = []
+        // for (s in filtered)
+        //     res.push({
+        //         [s]: filtered[s]
+        //     })
+        const res = [filtered];
+        const outFileName = process.argv[2];
 
-const res = []
-for(s in filtered)
-    res.push({
-        [s]: filtered[s]
-    })
-
-
-fs.writeFileSync(outFileName, JSON.stringify(res,null,2));
+        fs.writeFileSync(outFileName, JSON.stringify(res,null,2));
+    });
